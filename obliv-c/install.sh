e2d38450479194087ad277d7342e249b478c9b4f
Marcella Hastings <marcellahastings@gmail.com>
2018-11-08 16:56:21 -0500

    update oblivc install file

--- obliv-c/install.sh
+++ obliv-c/install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/samee/obliv-c
 
 cd obliv-c
-git reset --hard 3d6804ca0fd85868207a0ccbd2509ec064723ac2 
+git checkout -b MPCSOK 3d6804ca0fd85868207a0ccbd2509ec064723ac2 
 
 ./configure && make
