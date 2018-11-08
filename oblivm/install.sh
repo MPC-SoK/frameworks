d6e10232845b7fd43dc27189a56665612e97496a
Marcella Hastings <marcellahastings@gmail.com>
2018-11-08 17:04:01 -0500

    update oblivm install file

--- oblivm/install.sh
+++ oblivm/install.sh
@@ -1,6 +1,6 @@
 git clone https://github.com/oblivm/ObliVMLang
 
 cd ObliVMLang
-git reset --hard 50ed0fbb556a157c0c85440dabf141a3cf7384c5
+git checkout -b MPCSOK 50ed0fbb556a157c0c85440dabf141a3cf7384c5
 
 ./compile.sh
