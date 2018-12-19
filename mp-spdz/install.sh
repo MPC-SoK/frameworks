6dc4d43b46bb9dfc76046408a402b76d72f2b18f
Marcel Keller <mks.keller@gmail.com>
2018-12-19 15:38:03 +1100

    Use integrated MPIR.

--- mp-spdz/install.sh
+++ mp-spdz/install.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Install mpir
-wget http://mpir.org/mpir-3.0.0.tar.bz2
-tar -xjf mpir-3.0.0.tar.bz2
-rm mpir-3.0.0.tar.bz2
-cd mpir-3.0.0/
-./configure --enable-cxx
-make
-make install
-cd ..
-
-# Linking
-LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-ldconfig
-
 # Compile SPDZ with secure pre-processing
 git clone https://github.com/n1analytics/MP-SPDZ
 cd MP-SPDZ
@@ -23,6 +9,11 @@ echo USE_GF2N_LONG = 0 >> CONFIG.mine
 echo MOD = -DMAX_MOD_SZ=6 >> CONFIG.mine
 echo ARCH = -march=native >> CONFIG.mine
 
-make
+make mpir || exit 1
+make || exit 1
 mkdir Player-Data
 Scripts/setup-ssl.sh
+
+# Linking
+LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
+ldconfig
