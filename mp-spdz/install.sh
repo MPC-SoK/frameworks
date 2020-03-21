aa63d8dc5fe289a5535c86c8edcf9607bd756b47
Marcel Keller <mks.keller@gmail.com>
2020-03-22 00:55:43 +1100

    Update to 0.1.5.

--- mp-spdz/install.sh
+++ mp-spdz/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -L https://github.com/data61/MP-SPDZ/releases/download/v0.1.3/mp-spdz-0.1.3.tar.xz | tar xJv
-mv mp-spdz-0.1.3 MP-SPDZ
+curl -L https://github.com/data61/MP-SPDZ/releases/download/v0.1.5/mp-spdz-0.1.5.tar.xz | tar xJv
+mv mp-spdz-0.1.5 MP-SPDZ
 
 cd MP-SPDZ
 Scripts/tldr.sh
