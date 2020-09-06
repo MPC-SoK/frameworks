6d95730e80c14f63f1a8bbeb62ea5c85587613da
Xiao Wang <wangxiao1254@gmail.com>
2020-09-06 17:16:15 -0500

    minor

--- emp/ag_test/test/mult3.ag2pc.cpp
+++ emp/ag_test/test/mult3.ag2pc.cpp
@@ -7,7 +7,7 @@ using namespace emp;
 //const string circuit_file_location = macro_xstr(EMP_CIRCUIT_PATH);
 void test(int party, NetIO* io, string name, string check_output = "") {
     // read in the circuit from the location where it was generated
-	string file = "/root/emp-sh2pc/build/" + name;
+	string file = "./" + name;
         cout << file << endl;
 	CircuitFile cf(file.c_str());
     //
