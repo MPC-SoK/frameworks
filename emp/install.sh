eeb1a317ef96102b81e111d98319911f5b03393d
Xiao Wang <wangxiao1254@gmail.com>
2020-09-06 17:25:03 -0500

    minor

--- emp/install.sh
+++ emp/install.sh
@@ -2,3 +2,6 @@
 
 wget https://raw.githubusercontent.com/emp-toolkit/emp-readme/master/scripts/install.py
 python install.py -install -tool -ot -sh2pc -ag2pc
+
+cd sh_test && cmake . && make && cd ..
+cd ag_test && cmake . && make && cd ..
