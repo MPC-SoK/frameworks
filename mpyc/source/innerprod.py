aa9d1f42a71cfee5b6e63482082ffdbca3b1b758
niekbouman <niekbouman@gmail.com>
2020-09-10 09:33:05 +0200

    Update MPyC inner_product to use in_prod function
    in_prod is the recommended approach to compute an inner product;
    by linearity of Shamir's LSSS, we only perform degree reduction (resharing) after taking the inner product, thus on a single element.

--- mpyc/source/innerprod.py
+++ mpyc/source/innerprod.py
@@ -6,27 +6,29 @@ async def main():
     # initialize mpc, define secure int type
     LEN = 10
     await mpc.start()
-    secint = mpc.SecInt(64)
+    secint = mpc.SecInt(64) # work in a 64-bit prime field
 
-    # initialize inputs
-    values = [np.random.randint(1,1000) for _ in range(LEN)]
+    # party 0 samples the inputs locally... 
+    if mpc.pid == 0:
+        vec1 = [np.random.randint(1,1000) for _ in range(LEN)]
+        vec2 = [np.random.randint(1,1000) for _ in range(LEN)]
 
-    n = len(mpc.parties)
-    inputs = mpc.input([secint(v)for v in values], senders=list(range(n)))
+    # ...and secret-shares them with the others
+    result_type = [secint()] * LEN
+    sec_vec1 = mpc.input([secint(v) for v in vec1] if mpc.pid == 0 else result_type, senders=0)
+    sec_vec2 = mpc.input([secint(v) for v in vec2] if mpc.pid == 0 else result_type, senders=0)
 
-    # compute pairwise products
-    prod = inputs[0]
-    for inp in inputs[1:]:
-        prod = mpc.schur_prod(prod, inp)
-    ip = mpc.sum(prod)
+    # compute inner product
+    ip = mpc.in_prod(sec_vec1, sec_vec2)
    
-    # output result
+    # output result (to everybody)
     result = await mpc.output(ip)
     print("result:", result)
+    if mpc.pid == 0:
+        assert(result == np.dot(vec1, vec2))
     await mpc.shutdown()
 
 
 if __name__ == '__main__':
     
     mpc.run(main())
-
