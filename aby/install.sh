ea91640f126858314273f2dd4107a868df7a435e
Juri Dispan <juri.dispan@uni-ulm.de>
2023-05-12 19:11:06 +0200

    Bump ABY version

--- aby/install.sh
+++ aby/install.sh
@@ -3,11 +3,7 @@
 # get ABY
 git clone --recursive https://github.com/encryptogroup/ABY.git
 cd ABY
-git checkout -b MPCSOK 08baa853de76a9070cb8ed8d41e96569776e4773
-# We tried ABY's latest commit (as of May 2023) d8e69414d091cafc007e65a03ef30768ebaf723d,
-# however, the crosstabs example does no longer work under this version
-# (giving differing expected and actual results).
-# The reason for this behaviour is not clear.
+git checkout -b MPCSOK d8e69414d091cafc007e65a03ef30768ebaf723d
 
 # copy our working examples 
 for EX in mult3 crosstabs innerprod
