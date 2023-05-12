ab9054a5dbf03f090cb66c63a911bdc57c5bf027
Juri Dispan <juri.dispan@uni-ulm.de>
2023-05-12 18:39:53 +0200

    add comment to install.sh why ABY version can't be bumped

--- aby/install.sh
+++ aby/install.sh
@@ -4,6 +4,10 @@
 git clone --recursive https://github.com/encryptogroup/ABY.git
 cd ABY
 git checkout -b MPCSOK 08baa853de76a9070cb8ed8d41e96569776e4773
+# We tried ABY's latest commit (as of May 2023) d8e69414d091cafc007e65a03ef30768ebaf723d,
+# however, the crosstabs example does no longer work under this version
+# (giving differing expected and actual results).
+# The reason for this behaviour is not clear.
 
 # copy our working examples 
 for EX in mult3 crosstabs innerprod
