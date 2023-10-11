efcee5f711ed5ddb5ed3ee78e9a9f3e4aa377601
Juri Dispan <juri.dispan@uni-ulm.de>
2023-10-11 18:48:33 +0200

    Make Emp compile again

--- emp/install.sh
+++ emp/install.sh
@@ -3,5 +3,6 @@
 wget https://raw.githubusercontent.com/emp-toolkit/emp-readme/master/scripts/install.py
 # We don't set "-install" because our docker image already comes with the necessary dependencies.
 # See: https://github.com/emp-toolkit/emp-readme/blob/master/scripts/install.py#L12
-python3 install.py --tool --ot --sh2pc --ag2pc
+python3 install.py -install --tool --ot --sh2pc --ag2pc
+
 
