dc8b93aeb56e9e495bc7bb5c3f6d90b8b808b4d2
Juri Dispan <juri.dispan@uni-ulm.de>
2024-01-24 17:03:46 +0100

    SPDZ: fix xtabs example

--- spdz/run_example.sh
+++ spdz/run_example.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+set -e
+
 EX=$1
 
 mkdir -p Programs/InputData
@@ -15,9 +19,18 @@ if [ $EX = mult3 ]; then
     ./Player-Online.x 0 $EX & ./Player-Online.x 1 $EX & ./Player-Online.x 2 $EX
 
 elif [ $EX = innerprod ]; then
+    # This example is currently not working. For unknown reasons, it produces wrong outputs.
     ./compile.py -p 128 $EX
     printf "1\n14\n10\n5\n" > Programs/InputData/$EX.P0
     printf "1\n12\n64\n3\n" > Programs/InputData/$EX.P1
+    ./gen_input_fp.x -N 2 -i ./Programs/InputData/$EX.P0 -o ./Player-Data/Private-Input-0
+    ./gen_input_fp.x -N 2 -i ./Programs/InputData/$EX.P1 -o ./Player-Data/Private-Input-1
+
+    ./Server.x 2 5000 &
+    ./Player-Online.x 0 $EX & ./Player-Online.x 1 $EX
+
+elif [ $EX = xtabs ]; then
+    ./compile.py -p 128 $EX
     ./gen_input_fp.x -N 2 -i ./Programs/InputData/$EX.P0 -o ./Player-Data/Private-Input-0
     ./gen_input_fp.x -N 2 -i ./Programs/InputData/$EX.P1 -o ./Player-Data/Private-Input-1
 
